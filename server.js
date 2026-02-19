3483fa428c624191dbd6318f5b55af472d9b6a92
samshaalan <samshaalan@hotmail.com>
2026-02-19 20:27:15 +0100

    add initial endpoints

--- server.js
+++ server.js
@@ -8,21 +8,76 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", (req, res) => {
-  cn.query("SELECT * FROM filmer", (err, data) => {
+//Hämta alla produkter
+app.get("/products", (req, res) => {
+  cn.query("SELECT * FROM products", (err, data) => {
     res.send(data);
   });
 });
 
 app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  cn.query("SELECT * FROM products", (err, data) => {
-    res.send(data);
-  });
+app.get("/products/:id", (req, res) => {
+  cn.query(
+    `SELECT * FROM products WHERE product_id = ?`,
+    req.params.id,
+    (err, data) => {
+      res.send(data);
+    },
+  );
 });
 
-app.get("/customer/orders/:id", (res, req) => {
+/* app.get("/customer/orders/:id", (req, res) => {
+  cn.query(
+    `SELECT * FROM orders
+    JOIN order_items ON orders.order_id=order_items.order_id
+    JOIN products ON products.product_id=order_items.product_id
+    WHERE order_id = ?`,
+    req.params.id,
+    (err, data) => {
+      res.send(data);
+    },
+  );
+}); */
+
+app.get("/customers/:id/orders", (req, res) => {
+  cn.query(
+    `SELECT 
+      orders.order_id,
+      orders.order_date,
+      orders.total_price,
+      orders.order_status,
+      products.product_name,
+      products.listing_price,
+      order_items.product_quantity,
+      order_items.item_price
+    FROM orders
+    JOIN order_items ON orders.order_id = order_items.order_id
+    JOIN products ON products.product_id = order_items.product_id
+    WHERE orders.customer_id = ?`,
+    [req.params.id],
+    (err, data) => {
+      if (err) return res.status(500).send(err);
+      res.send(data);
+    },
+  );
+});
+
+app.get("/categories/:id/products", (req, res) => {
+  cn.query(
+    `SELECT products.* FROM product_categories
+    JOIN products ON products.product_id = product_categories.product_id
+    JOIN categories ON categories.category_id = product_categories.category_id
+    WHERE categories.category_id = ?`,
+    [req.params.id],
+    (err, data) => {
+      if (err) return res.status(500).send(err);
+      res.send(data);
+    },
+  );
+});
+
+/* app.get("/customer/orders/:id", (res, req) => {
   cn.query(
     `SELECT * FROM orders
     JOIN order_items ON orders.orders_id=orders_items.orders_id
@@ -35,7 +90,7 @@ app.get("/customer/orders/:id", (res, req) => {
       res.send(data);
     },
   );
-});
+}); */
 
 app.post("/admin/create-products", (req, res) => {
   cn.query(
@@ -46,5 +101,15 @@ app.post("/admin/create-products", (req, res) => {
   res.send("klart!");
 });
 
+/* Som kund vill jag kunna söka efter produkter så att jag snabbt kan hitta specifika varor */
+app.get("/products", (req, res) => {
+  cn.query("SELECT * FROM products", (err, data) => {
+    res.send(data);
+  });
+});
+
 /* ett program som körs som vänta och väntar på request */
 app.listen(PORT);
+
+/* GET /products?category=laptops&minPrice=5000&sort=price
+GET /users?role=admin&limit=10&offset=20 */
