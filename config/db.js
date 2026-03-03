ea2a1024d1381321a83d6c8fcea0bca0ea21edc2
nils-fjp <nils.fj.paulsson@outlook.com>
2026-03-03 12:31:55 +0100

    update db connection, routes, and controllers to use pool, async/await, and try/catch

--- config/db.js
+++ config/db.js
@@ -1,5 +1,33 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+(async () => {
+  try {
+    const connection = await pool.getConnection();
+    console.log("MySQL createPool() succeeded!");
+    connection.release();
+  } catch (err) {
+    console.error("Connection error:", err);
+    process.exit(1);
+  }
+})();
+
+module.exports = pool;
+
+/* 
+
+// const mysql = require("mysql2");
+// require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -13,7 +41,8 @@ connection.connect((err) => {
     console.error("Connection error:", err);
     process.exit(1);
   }
-  console.log("MySQL connected!");
+  console.log("MySQL createConnection() succeeded!");
 });
 
 module.exports = connection;
+*/
